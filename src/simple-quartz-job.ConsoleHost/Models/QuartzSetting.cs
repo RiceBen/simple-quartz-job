cc38f0a27cb83b0a46ce52578672c1d5de91a531
benjamin_fan <benjamin_fan@trendmicro.com>
2023-02-07 23:00:19 +0800

    init repo

---
+++ src/simple-quartz-job.ConsoleHost/Models/QuartzSetting.cs
@@ -0,0 +1,10 @@
+﻿namespace simple_quartz_job.ConsoleHost.Models;
+
+public class QuartzSetting
+{
+    public string Cron { get; set; }
+    
+    public string Description { get; set; }
+
+    public bool DisallowConcurrentExecution { get; set; }
+}
---
+++ src/simple-quartz-job/HealthyCheckJob.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace simple_quartz_job;
+
+public class HealthyCheckJob : IJob
+{
+    private readonly ILogger<HealthyCheckJob> _logger;
+
+    public HealthyCheckJob(ILogger<HealthyCheckJob> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        _logger.LogInformation(
+            $"[{DateTime.UtcNow.ToString("u")}][{context.Trigger.JobKey}] Healthy.");
+
+        await Task.CompletedTask;
+    }
+}
