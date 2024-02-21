be0be01acac0892326074e01f8fa1b44eb051966
Rice Ben <1697448+RiceBen@users.noreply.github.com>
2024-02-21 22:24:49 +0800

    init repo

---
+++ src/simple-quartz-job.ConsoleHost/Extensions/ServiceCollectionQuartzConfiguratorExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Quartz;
+using simple_quartz_job.ConsoleHost.Models;
+
+namespace simple_quartz_job.ConsoleHost.Extensions;
+
+public static class ServiceCollectionQuartzConfiguratorExtensions
+{
+    public static void AddJobAndTrigger<T>(
+        this IServiceCollectionQuartzConfigurator quartz,
+        IConfiguration config)
+        where T : IJob
+    {
+        var jobName = typeof(T).Name;
+        var configKey = $"Quartz:{jobName}";
+
+        var jobSettings = config.GetSection(configKey).Get<QuartzSetting>();
+
+        if (jobSettings is null)
+        {
+            throw new Exception($"No Cron schedule found for job in configuration at {configKey}");
+        }
+
+        var jobKey = new JobKey(jobName);
+        quartz.AddJob<T>(opts =>
+            opts.WithIdentity(jobKey)
+                .WithDescription(jobSettings.Description)
+                .DisallowConcurrentExecution(jobSettings.DisallowConcurrentExecution));
+
+        quartz.AddTrigger(opts => opts
+            .ForJob(jobKey)
+            .WithIdentity(jobName + "-trigger")
+            .WithCronSchedule(jobSettings.Cron));
+    }
+}
---
+++ src/simple-quartz-job.ConsoleHost/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+using simple_quartz_job;
+using simple_quartz_job.ConsoleHost.Extensions;
+
+Debug.WriteLine($"[{DateTime.UtcNow.ToString("u")}] Start {Process.GetCurrentProcess().MainModule?.ModuleName}!");
+
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("Config/appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureHostConfiguration(configHost => { configHost.AddConfiguration(config); })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddQuartz(quartz =>
+        {
+            quartz.SchedulerId = hostContext.Configuration["Scheduler:Id"] ??
+                                 $"{Process.GetCurrentProcess().MainModule?.ModuleName}";
+            quartz.UseMicrosoftDependencyInjectionJobFactory();
+            quartz.UseSimpleTypeLoader();
+            quartz.UseInMemoryStore();
+            quartz.UseDefaultThreadPool(tp =>
+            {
+                tp.MaxConcurrency = int.Parse(hostContext.Configuration["Scheduler:MaxConcurrency"] ?? "10");
+            });
+
+            quartz.AddJobAndTrigger<HealthyCheckJob>(hostContext.Configuration);
+        });
+
+        services.AddQuartzHostedService(options =>
+        {
+            options.WaitForJobsToComplete = true;
+            options.AwaitApplicationStarted = true;
+        });
+    }).Build().Run();
